70303fab22dd22022db329a97901e0ecfa48d139
Han Zhu <hanzhu@microsoft.com>
2016-04-21 05:49:14 +0000

    fix FindUnitTest++ cmake module

--- Microsoft.WindowsAzure.Storage/cmake/Modules/FindUnitTest++.cmake
+++ Microsoft.WindowsAzure.Storage/cmake/Modules/FindUnitTest++.cmake
@@ -11,7 +11,7 @@ include(FindPkgConfig)
 include(FindPackageHandleStandardArgs)
 
 # Use pkg-config to get hints about paths
-pkg_check_modules(UnitTest++_PKGCONF REQUIRED unittest++)
+pkg_check_modules(UnitTest++_PKGCONF QUIET unittest++)
 
 # Include dir
 find_path(UnitTest++_INCLUDE_DIR
@@ -19,14 +19,19 @@ find_path(UnitTest++_INCLUDE_DIR
   PATHS
   	${UnitTest++_PKGCONF_INCLUDE_DIRS}
   	${CMAKE_SOURCE_DIR}/tests/UnitTest++/src
+	/usr/local/include
+  PATH_SUFFIXES
+	unittest++
+	UnitTest++
 )
 
 # Library
 find_library(UnitTest++_LIBRARY
-  NAMES unittest++
+  NAMES unittest++ UnitTest++
   PATHS 
   	${UnitTest++_PKGCONF_LIBRARY_DIRS}
   	${CMAKE_SOURCE_DIR}/tests/UnitTest++
+	/usr/local/lib
 )
 
 set(UnitTest++_PROCESS_LIBS UnitTest++_LIBRARY)

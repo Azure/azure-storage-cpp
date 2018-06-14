644f24608d031f79bf5841e32c31ffd28f73c46a
Tank Tang <kat@microsoft.com>
2018-06-14 17:11:36 +0800

    Enabled CMake on Windows

--- Microsoft.WindowsAzure.Storage/cmake/Modules/FindCasablanca.cmake
+++ Microsoft.WindowsAzure.Storage/cmake/Modules/FindCasablanca.cmake
@@ -7,26 +7,39 @@ find_package(PkgConfig)
 
 include(LibFindMacros)
 
-# Include dir
-find_path(CASABLANCA_INCLUDE_DIR
-  NAMES
-    cpprest/http_client.h
-  PATHS 
-    ${CASABLANCA_PKGCONF_INCLUDE_DIRS}
-    ${CASABLANCA_DIR}
-    $ENV{CASABLANCA_DIR}
-    /usr/local/include
-    /usr/include
-    ../../casablanca
-  PATH_SUFFIXES 
-    Release/include
-    include
-)
+if(WIN32)
+  find_package(cpprestsdk)
+  
+  if(cpprestsdk_FOUND)
+    set(CASABLANCA_LIBRARY cpprestsdk::cpprest)
+    set(CASABLANCA_PROCESS_LIBS CASABLANCA_LIBRARY)
+    set(CASABLANCA_PROCESS_INCLUDES CASABLANCA_INCLUDE_DIR)
+    libfind_process(CASABLANCA)
+    return()
+  endif()
+else()
+  # Include dir
+  find_path(CASABLANCA_INCLUDE_DIR
+    NAMES
+      cpprest/http_client.h
+    PATHS 
+      ${CASABLANCA_PKGCONF_INCLUDE_DIRS}
+      ${CASABLANCA_DIR}
+      $ENV{CASABLANCA_DIR}
+      /usr/local/include
+      /usr/include
+      ../../casablanca
+    PATH_SUFFIXES 
+      Release/include
+      include
+  )
+endif()
 
 # Library
 find_library(CASABLANCA_LIBRARY
   NAMES 
     cpprest
+    cpprest_2_9.lib
   PATHS 
     ${CASABLANCA_PKGCONF_LIBRARY_DIRS}
     ${CASABLANCA_DIR}

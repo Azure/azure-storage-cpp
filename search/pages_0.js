3229c9d834d14014769d428352990f10959a7ea8
Tank Tang <kat@microsoft.com>
2018-10-19 17:12:00 +0800

    Docs for Azure Storage Client Library for C++ v5.2.0

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['azure_20storage_20client_20library_20for_20c_2b_2b_20_285_2e1_2e1_29',['Azure Storage Client Library for C++ (5.1.1)',['../index.html',1,'']]]
+  ['azure_20storage_20client_20library_20for_20c_2b_2b_20_285_2e2_2e0_29',['Azure Storage Client Library for C++ (5.2.0)',['../index.html',1,'']]]
 ];

c9c5f1edabac90f1437715eb6b4726b097b963e6
Tank Tang <kat@microsoft.com>
2019-02-01 11:31:15 +0800

    Docs for Azure Storage Client Library for C++ v6.0.0

--- search/classes_11.js
+++ search/classes_11.js
@@ -7,5 +7,6 @@ var searchData=
   ['table_5fquery',['table_query',['../classazure_1_1storage_1_1table__query.html',1,'azure::storage']]],
   ['table_5frequest_5foptions',['table_request_options',['../classazure_1_1storage_1_1table__request__options.html',1,'azure::storage']]],
   ['table_5fresult',['table_result',['../classazure_1_1storage_1_1table__result.html',1,'azure::storage']]],
-  ['table_5fshared_5faccess_5fpolicy',['table_shared_access_policy',['../classazure_1_1storage_1_1table__shared__access__policy.html',1,'azure::storage']]]
+  ['table_5fshared_5faccess_5fpolicy',['table_shared_access_policy',['../classazure_1_1storage_1_1table__shared__access__policy.html',1,'azure::storage']]],
+  ['timer_5fhandler',['timer_handler',['../classazure_1_1storage_1_1core_1_1timer__handler.html',1,'azure::storage::core']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['azure_20storage_20client_20library_20for_20c_2b_2b_20_285_2e2_2e0_29',['Azure Storage Client Library for C++ (5.2.0)',['../index.html',1,'']]]
+  ['azure_20storage_20client_20library_20for_20c_2b_2b_20_286_2e0_2e0_29',['Azure Storage Client Library for C++ (6.0.0)',['../index.html',1,'']]]
 ];

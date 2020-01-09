697bf2a08b5a75077bcef5746472e7a63cdb52ab
Jinming Hu <jinmhu@microsoft.com>
2020-01-09 12:19:31 +0800

    Docs for Azure Storage Client Library for C++ v7.1.0

--- search/all_14.js
+++ search/all_14.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['validate_5fcertificates',['validate_certificates',['../classazure_1_1storage_1_1request__options.html#aa5d5a868bf0b03ff66f013b071474a34',1,'azure::storage::request_options']]],
   ['value',['value',['../classazure_1_1storage_1_1sequence__number.html#a096f908a189407455a32f287dc841686',1,'azure::storage::sequence_number']]],
   ['values',['values',['../structazure_1_1storage_1_1blob__listing__details.html#aceeac9a2f3c1077ecb811dc0f9548bc9',1,'azure::storage::blob_listing_details::values()'],['../structazure_1_1storage_1_1container__listing__details.html#a0f5198f2b6837897e40e96e07624f9e4',1,'azure::storage::container_listing_details::values()']]],
   ['version',['version',['../classazure_1_1storage_1_1service__properties_1_1logging__properties.html#a8d530d0165b9f15008075a95007cf00b',1,'azure::storage::service_properties::logging_properties::version()'],['../classazure_1_1storage_1_1service__properties_1_1metrics__properties.html#a06a2ffe285f95061f0acbdee69cd0b26',1,'azure::storage::service_properties::metrics_properties::version()']]]
--- search/classes_12.js
+++ search/classes_12.js
@@ -1,11 +1,4 @@
 var searchData=
 [
-  ['xml_5fdocument_5fwrapper',['xml_document_wrapper',['../classazure_1_1storage_1_1core_1_1xml_1_1xml__document__wrapper.html',1,'azure::storage::core::xml']]],
-  ['xml_5felement_5fwrapper',['xml_element_wrapper',['../classazure_1_1storage_1_1core_1_1xml_1_1xml__element__wrapper.html',1,'azure::storage::core::xml']]],
-  ['xml_5freader',['xml_reader',['../classazure_1_1storage_1_1core_1_1xml_1_1xml__reader.html',1,'azure::storage::core::xml']]],
-  ['xml_5ftext_5freader_5fwrapper',['xml_text_reader_wrapper',['../classazure_1_1storage_1_1core_1_1xml_1_1xml__text__reader__wrapper.html',1,'azure::storage::core::xml']]],
-  ['xml_5fwriter',['xml_writer',['../classazure_1_1storage_1_1core_1_1xml_1_1xml__writer.html',1,'azure::storage::core::xml']]],
-  ['xmlstring_5fistream',['xmlstring_istream',['../classazure_1_1storage_1_1core_1_1xml_1_1xmlstring__istream.html',1,'azure::storage::core::xml']]],
-  ['xmlstring_5fostream',['xmlstring_ostream',['../classazure_1_1storage_1_1core_1_1xml_1_1xmlstring__ostream.html',1,'azure::storage::core::xml']]],
-  ['xmlstring_5fstream',['xmlstring_stream',['../classazure_1_1storage_1_1core_1_1xml_1_1xmlstring__stream.html',1,'azure::storage::core::xml']]]
+  ['user_5fdelegation_5fkey',['user_delegation_key',['../structazure_1_1storage_1_1user__delegation__key.html',1,'azure::storage']]]
 ];
